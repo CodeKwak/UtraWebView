5d126d536f11aebeaf4f5742ae916efd35359f4b
CodeKwak <prncfoli@gmail.com>
2016-01-08 18:58:12 +0000

    the new UtraWebView
    Hey Guys. This is the fastest web browser ever to developed. but still in process ... ..

---
+++ UtraWebView.java
@@ -0,0 +1,7 @@
+ 
+ /** This Class is the future of Android WebView implementation techonology
+ *   {@link UtraListener} -  this is the future of UtraFast interfaces
+ /
+protected void UtraWebView extends AdvancedWebView implements UtraListener { 
+
+}
